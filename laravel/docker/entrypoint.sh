5a8dab3126be329bc22f1fba86cc5ce0f31de031
Walid <walid.idrissi.labs@gmail.com>
2026-07-25 20:26:27 +0100

    Deployment Container : force php artisan migrate on container startup

--- laravel/docker/entrypoint.sh
+++ laravel/docker/entrypoint.sh
@@ -4,4 +4,7 @@ set -e
 export PORT="${PORT:-8080}"
 envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
+php artisan config:cache
+php artisan migrate --force
+
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
